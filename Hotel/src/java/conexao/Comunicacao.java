4096abb95b45866002b30ca72de2db4be089587a
Nikollas <nikollasferreira@hotmail.com>
2016-10-06 09:43:59 -0300

    asdasd

--- Hotel/src/java/conexao/Comunicacao.java
+++ Hotel/src/java/conexao/Comunicacao.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,10 +11,7 @@ import java.sql.SQLException;
 import javax.faces.bean.ManagedBean;
 import javax.faces.component.html.HtmlForm;
 import javax.faces.component.html.HtmlOutputText;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import model.*;
 
 /**
@@ -42,7 +39,7 @@ public class Comunicacao {
                 return opc;
             } else {
                 opt.setValue("Login inválido! Tente novamente.");
-                form.setStyle("display:block; ");
+                form.setStyle("visibility:visible; ");
                 return "";
             }
         } else {
